3f49b918cbb724850ac6b3f466817748e839c3e6
Heather <hlmathies@gmail.com>
2017-12-27 15:42:19 -0500

    images display

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Nav from "./components/Nav";
+import cards from "./cards.json";
+import "./App.css";
 
 class App extends Component {
+  // Setting this.state.cards to the cards json array
+  state = {
+    cards
+  };
+
+  removecard = id => {
+    // Filter this.state.cards for cards with an id not equal to the id being removed
+    const cards = this.state.cards.filter(card => card.id !== id);
+    // Set this.state.cards equal to the new cards array
+    this.setState({ cards });
+  };
+
+  // Map over this.state.cards and render a Card component for each card object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper>
+        <Nav>Clicky Game</Nav>
+        {this.state.cards.map(card => (
+          <Card
+            id={card.id}
+            key={card.id}
+            image={card.image}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
