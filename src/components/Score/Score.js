36b005e70bfc13390bf9dd1c71678cd1430832d0
Heather <hlmathies@gmail.com>
2017-12-29 20:37:50 -0500

    Win or Loss displays in browser

--- src/components/Score/Score.js
+++ src/components/Score/Score.js
@@ -1,21 +1,27 @@
 import React from "react";
 import "./Score.css";
-//maybe use this component to keep score
-// const Score = props => <h1 className="score">{props.children}</h1>;
-//
-// export default Score;
+
 
 const Score = props => (
-  <div className="score-count">
-    <div className="total-count">
-      <span>Score: </span>
-      <span>{props.total}</span>
-    </div>
-    <div>
-      <span>Goal: </span>
-      <span>{props.goal}</span>
-    </div>
+  <div className="gameScore">
+    <h2 className="score">Your Score: {props.total}</h2>
+    <h3 className="goal">Goal to win: 9</h3>
+    <h2 className="status">{props.status}</h2>
   </div>
 );
 
 export default Score;
+
+
+// import React from "react";
+// import "./Card.css";
+//
+// //this component is only rendering the images to the screen and can be a simple render function
+// const Card = props => (
+//   <div className="card img-container">
+//       <img alt={props.name} src={props.image} id={props.id}
+//         onClick={() => props.shuffleScoreCard(props.id)} className='shuffleScore'/>
+//   </div>
+// );
+//
+// export default Card;
