472c3046668fa250e23eb830c336c33f3bdf0d78
Heather <hlmathies@gmail.com>
2017-12-29 19:22:33 -0500

    Game is shuffling images and keeping score

--- src/App.js
+++ src/App.js
@@ -8,28 +8,34 @@ import "./App.css";
 class App extends Component {
   // Setting this.state.pups to the cards json array
   state = {
-    pups
+    pups,
+    clickedPuppyIds: [],
+    score: 0
   };
+
   //shuffle the pup cards in the browser when clicked
-    shuffleScoreCard = id => {
-        for (let i = pups.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            [pups[i], pups[j]] = [pups[j], pups[i]];
-        }
-        this.setState({ pups });
-  //the below console is coming up undefined.  Trying to figure out why
-        // console.log(this.pups[id]);
-        console.log(pups.shuffleScoreCard);
-        //each time a puppy image is clicked, checked the id to see if it's already been clicked
-      //if the puppy clicked has already been clicked return
-      //else increase score by one and reshuffle
-          // if (this.pups[id] !== pups[id]){
-          //   shuffleScoreCard();
-          // } else {
-          //   return;
-          // };
+  shuffleScoreCard = id => {
+    let clickedPuppyIds = this.state.clickedPuppyIds;
+
+    if(clickedPuppyIds.includes(id)){
+      console.log('You Lose');
+      this.setState({ clickedPuppyIds: [], score: 0 });
+      return;
+    }else{
+      clickedPuppyIds.push(id)
+
+      if(clickedPuppyIds.length === 9){
+        console.log('You Win');
+      }
+
+      this.setState({ pups, clickedPuppyIds, score: clickedPuppyIds.length });
+
+      for (let i = pups.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [pups[i], pups[j]] = [pups[j], pups[i]];
+      }
     }
-    //^^^^^this if statement isn't working
+  }
 
   // Map over this.state.cards and render a Card component for each card object
   render() {
@@ -41,7 +47,7 @@ class App extends Component {
             Try not to click the same image twice!
           </p>
         </header>
-        <Score total={0}
+        <Score total={this.state.score}
                goal={9}/>
       <Wrapper>
         {this.state.pups.map(puppy => (
