50c19efe0f89f7393f03bcd0f9cedb65ab31cf90
Heather <hlmathies@gmail.com>
2018-01-06 16:08:48 -0500

    Fix image layout

--- src/components/Score/Score.js
+++ src/components/Score/Score.js
@@ -6,8 +6,7 @@ import Arrow from 'react-icons/lib/fa/caret-right';
 const Score = props => (
   <div className="gameScore">
     <h2 className="score">Your Score <Arrow /> {props.total}</h2>
-    <h3 className="goal">Goal to win: 9</h3>
-    <h2 className="status">{props.status}</h2>
+    <h3 className="status">{props.status}</h3>
   </div>
 );
 
