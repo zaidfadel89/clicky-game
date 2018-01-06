daf6e54afb6347ee7a929c8fe64e3af5865fe574
Heather <hlmathies@gmail.com>
2018-01-06 16:20:52 -0500

    Format layout for heroku

--- src/components/Score/Score.js
+++ src/components/Score/Score.js
@@ -5,7 +5,7 @@ import Arrow from 'react-icons/lib/fa/caret-right';
 //stateless component
 const Score = props => (
   <div className="gameScore">
-    <h2 className="score">Your Score <Arrow /> {props.total}</h2>
+    <h3 className="score">Your Score <Arrow /> {props.total}</h3>
     <h3 className="status">{props.status}</h3>
   </div>
 );
