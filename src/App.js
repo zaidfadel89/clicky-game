7066b37983b31169a7fba3d44210b90dfd8b27d7
Zaid Fadel <zaid.fadel89@gmail.com>
2019-11-14 21:01:50 -0600

    clickyGame

--- src/App.js
+++ src/App.js
@@ -19,41 +19,50 @@ class App extends Component {
   shuffleScoreCard = id => {
     let clickedPuppyIds = this.state.clickedPuppyIds;
 
-    if(clickedPuppyIds.includes(id)){
-      this.setState({ clickedPuppyIds: [], score: 0, status:  "Game Over! You lost. Click to play again!" });
+    if (clickedPuppyIds.includes(id)) {
+      this.setState({
+        clickedPuppyIds: [],
+        score: 0,
+        status: "Game Over Click to play again!"
+      });
       return;
-    }else{
-      clickedPuppyIds.push(id)
+    } else {
+      clickedPuppyIds.push(id);
 
-      if(clickedPuppyIds.length === 8){
-        this.setState({score: 8, status: "You Won! Great Job, Smartie! Click to play again!", clickedPuppyIds: []});
-        console.log('You Win');
+      if (clickedPuppyIds.length === 8) {
+        this.setState({
+          score: 8,
+          status: "You Won! Click to play again!",
+          clickedPuppyIds: []
+        });
+        console.log("You Win");
         return;
       }
 
-      this.setState({ pups, clickedPuppyIds, score: clickedPuppyIds.length, status: " " });
+      this.setState({
+        pups,
+        clickedPuppyIds,
+        score: clickedPuppyIds.length,
+        status: " "
+      });
 
       for (let i = pups.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
         [pups[i], pups[j]] = [pups[j], pups[i]];
       }
     }
-  }
+  };
 
-  // Map over this.state.cards and render a Card component for each card object
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">The Clickster</h1>
-          <p className="App-intro">
-            Try not to click the same image twice!
-          </p>
+          <center>
+            {" "}
+            <h1 className="App-title">The Clickster</h1>
+          </center>
         </header>
-        <Score total={this.state.score}
-               goal={8}
-               status={this.state.status}
-               />
+        <Score total={this.state.score} goal={8} status={this.state.status} />
         <Wrapper>
           {this.state.pups.map(puppy => (
             <Card
@@ -64,11 +73,7 @@ class App extends Component {
             />
           ))}
         </Wrapper>
-        <footer>
-          <p>Designed and built by Heather Mathies. You can find the
-          code<a href="https://github.com/hmathies/Clicky-Game" target="_blank" rel="noopener noreferrer"> here</a>.</p>
-        </footer>
-    </div>
+      </div>
     );
   }
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./Card.css";
 
-//this component is only rendering the images to the screen and can be a simple render function it is a
-//stateless component
 const Card = props => (
   <div className="card img-container hover">
-      <img alt={props.name} src={props.image} id={props.id}
-        onClick={() => props.shuffleScoreCard(props.id)} className='shuffleScore'/>
+    <img
+      alt={props.name}
+      src={props.image}
+      id={props.id}
+      onClick={() => props.shuffleScoreCard(props.id)}
+      className="shuffleScore"
+    />
   </div>
 );
 
